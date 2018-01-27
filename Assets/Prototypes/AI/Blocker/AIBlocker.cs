4c26468f7dc12988acaa4fef9017d35e3f207a1f
Jon Ander Hernandez <jonan.h@gmail.com>
2018-01-27 19:24:45 +0200

    Update to the prototypes

---
+++ Assets/Prototypes/AI/Blocker/AIBlocker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBlocker : MonoBehaviour 
+{
+    public NavMeshAgent enemyAgent;
+    public NavMeshAgent playerAgent;
+    public Transform target;
+
+    public float distanceThreadshold;
+
+    IEnumerator Start () 
+    {
+        StartCoroutine(BlockState());
+
+        yield break;
+    }
+
+    private IEnumerator BlockState()
+    {
+        while (true)
+        {
+            var distance =
+                (target.transform.position - playerAgent.transform.position).sqrMagnitude;
+
+            // Tries to get in between the target and the player
+            if (distance < distanceThreadshold * distanceThreadshold)
+            {
+                enemyAgent.destination = Vector3.Lerp(
+                    target.transform.position,
+                    playerAgent.transform.position, 0.5f);
+            }
+            else
+            {
+                var direction = playerAgent.transform.position - target.transform.position;
+
+                enemyAgent.destination =
+                    target.transform.position + direction.normalized * distanceThreadshold;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Prototypes/AI/States/AIStates.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIStates : MonoBehaviour 
+{
+    public enum State 
+    {
+        Idle,
+        Blocking,
+        Wandering,
+    }
+
+    [SerializeField]
+    private NavMeshAgent enemyAgent;
+
+    [SerializeField]
+    private NavMeshAgent playerAgent;
+
+    #region blocking fields
+    [SerializeField]
+    private Transform blockingTarget;
+    #endregion
+
+    #region wandering parameters
+    [SerializeField]
+    private float wanderingSpeed = 4;
+
+    [SerializeField]
+    private float wanderingDistance = 1;
+
+    [SerializeField]
+    private float wanderingDirectionChangePeriod = 1;
+    #endregion
+
+    #region private fields
+    private State state = State.Idle;
+    private Vector3 enemyEndPos;
+    private Vector3 playerEndPos;
+    private Coroutine lastCoroutine;
+    #endregion
+
+    IEnumerator Start () 
+    {
+        yield return null;
+
+        // Initial positions
+        enemyEndPos = transform.position;
+        playerEndPos = playerAgent.transform.position;
+        enemyAgent.destination = enemyAgent.transform.position;
+        
+        while (true)
+        {
+            // Evaluate condition to change the behaviour
+            var newState = State.Wandering;
+
+            // Stop previous behaviour if the state changes
+            if (newState != state && lastCoroutine != null)
+            {
+                StopCoroutine(lastCoroutine);
+            }
+
+            // Select new state
+            switch (state)
+            {
+                case State.Wandering:
+                    lastCoroutine = StartCoroutine(WanderingState(enemyAgent.transform.position));
+                    break;
+            }
+
+            state = newState;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator BlockingState()
+    {
+        yield return null;
+    }
+
+    private IEnumerator WanderingState(Vector3 aroundPosition)
+    {
+        enemyAgent.destination = aroundPosition;
+
+        float lastTime = Time.time;
+
+        while (true)
+        {
+            // One it reaches the previous destination, search for another randome place wehre to go
+            if (Mathf.Abs(lastTime - Time.time) > wanderingDirectionChangePeriod)
+            {
+                lastTime = Time.time;
+
+                var delta = Random.rotation * Vector3.right * wanderingDistance;
+
+                enemyAgent.speed = wanderingSpeed;
+                enemyAgent.destination = aroundPosition + delta;
+            }
+
+            yield return null;
+        }
+    }
+
+    private void BlockState()
+    {
+        var playerData = playerAgent.currentOffMeshLinkData;
+        var enemyData = enemyAgent.currentOffMeshLinkData;
+
+        var distance = Vector3.SqrMagnitude(playerEndPos - playerAgent.transform.position);
+        var isFarFromWhereIAmGoing = distance > 10;
+
+        enemyAgent.destination = playerAgent.destination;
+
+        /*
+        // Move
+        enemyAgent.transform.position = Vector3.MoveTowards(
+            enemyAgent.transform.position, 
+            enemyEndPos, 
+            enemyAgent.speed * Time.deltaTime);
+        */
+    }
+}
