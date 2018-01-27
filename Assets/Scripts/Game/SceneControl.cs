79f59ffd376b590d8dcc08532b62b09ed33f190e
ARVUTI\martin <martinviidik@gmail.com>
2018-01-27 15:44:51 +0200

    no message

---
+++ Assets/Scripts/Game/SceneControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour {
+    public IEnumerator ReloadScene(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public IEnumerator LoadScene(float delay, string scene){
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(scene);
+    }
+
+}
