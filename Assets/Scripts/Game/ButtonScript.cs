ee8fbf255c84da976fc5ec9491f2582292f73343
ARVUTI\martin <martinviidik@gmail.com>
2018-01-27 17:44:07 +0200

    rough main menu

---
+++ Assets/Scripts/Game/ButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour, IDeselectHandler, ISelectHandler , IPointerEnterHandler, IPointerExitHandler{
+
+	[SerializeField] GameObject deactivated;
+	[SerializeField] GameObject activated;
+
+	public void OnPointerEnter(PointerEventData eventData){
+		Selected();
+    }
+     public void OnSelect(BaseEventData eventData){
+		Selected();
+    }
+
+	public void OnDeselect(BaseEventData eventData){
+		Deselected();
+    }
+
+	public void OnPointerExit(BaseEventData eventData){
+		Deselected();
+	}
+
+	private void Selected(){
+		activated.SetActive(true);
+		deactivated.SetActive(false);
+	}
+
+	private void Deselected(){
+		activated.SetActive(false);
+		deactivated.SetActive(true);
+	}
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Deselected();
+    }
+}
---
+++ Assets/Scripts/Game/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Video;
+
+public class MainMenu : MonoBehaviour {
+	[SerializeField] GameObject menu;
+	public VideoPlayer vid;
+	private SceneControl scene;
+	void Start(){
+		menu.SetActive(false);
+		vid.GetComponent<VideoPlayer>().Play();
+		scene = gameObject.GetComponent<SceneControl>();
+		InvokeRepeating("checkOver", .1f,.1f);
+	}
+
+	private void checkOver(){
+		long playerCurrentFrame = vid.GetComponent<VideoPlayer>().frame;
+		long playerFrameCount = System.Convert.ToInt64(vid.GetComponent<VideoPlayer>().frameCount);
+		
+		if(playerCurrentFrame < playerFrameCount)
+		{
+			print ("VIDEO IS PLAYING");
+		}
+		else
+		{
+			print ("VIDEO IS OVER");
+			CancelInvoke("checkOver");
+			menu.SetActive(true);
+		}
+	}
+
+	public void PlayGame(){
+		scene.LoadScene(0.0f, "playerTest");
+	}
+
+	public void AboutMenu(){
+
+	}
+
+	public void ExitGame(){
+
+	}
+
+}
